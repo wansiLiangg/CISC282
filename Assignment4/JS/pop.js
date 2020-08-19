f1f888ee26cb5937e5f6c1b862779afdc794bbb9
wansiliang <69921677+wansiLiangg@users.noreply.github.com>
2020-08-19 17:51:23 -0400

    Add files via upload

---
+++ Assignment4/JS/pop.js
@@ -0,0 +1,20 @@
+// Wansi Liang
+
+$(document).ready(function() {
+	$('.thumbnail-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.find("img").attr('alt');
+			}
+		}
+	});
+});
